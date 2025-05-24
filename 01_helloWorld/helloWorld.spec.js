e3646a6350dda7714bdbb920cf252c64967e4071
Aerin Park <78767681+aerinpark@users.noreply.github.com>
2025-05-23 19:22:20 -0700

    fix helloWorld method to return a greeting
    Updated the return value of the method to pass the given test.

--- 01_helloWorld/helloWorld.spec.js
+++ 01_helloWorld/helloWorld.spec.js
@@ -1,7 +1,11 @@
+// require(): import the code from the javascript file
 const helloWorld = require('./helloWorld');
 
+// describe: body of the file where the test is run
 describe('Hello World', function() {
+  // test: describes what should be happening in plain english
   test('says "Hello, World!"', function() {
+    // expect: compares the output with the expected value
     expect(helloWorld()).toEqual('Hello, World!');
   });
 });
